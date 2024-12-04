b329ebcc0b61ca2f7cb804f7549839504d267afa
ibnLotfi <ibnlotfi@hotmail.fr>
2024-12-04 20:06:15 +0100

    Adding the filter get todo by priority

--- TodoClientApp/src/app/components/todo-board-header/todo-board-header.component.ts
+++ TodoClientApp/src/app/components/todo-board-header/todo-board-header.component.ts
@@ -1,20 +1,53 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { Priority } from '../../models/todo';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatOption } from '@angular/material/core';
+import { MatSelect, MatSelectChange } from '@angular/material/select';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-board-header',
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormField, MatLabel, MatOption, MatSelect],
   templateUrl: './todo-board-header.component.html',
   styleUrl: './todo-board-header.component.css'
 })
 export class TodoBoardHeaderComponent {
   today: Date = new Date();
+  priorityFilter: string = '';
+  public priorities = ['All'].concat(
+    Object.values(Priority).filter((value) => typeof value === 'string')
+  );
+  
+  form: FormGroup = new FormGroup({
+    priority: new FormControl('')
+  });
 
   @Output() openCreateTodoDialog: EventEmitter<any> = new EventEmitter();
+  @Output() refreshTodos: EventEmitter<any> = new EventEmitter();
 
   callOpenCreateTodoDialog(){
     this.openCreateTodoDialog.emit();
   }
+
+  // callRefreshTodos(event?: MatSelectChange){
+  //   // this.priorityFilter = ((this.form.value.priority == null) || (this.form.value.priority == undefined)) ? '' : this.form.value.priority.valueOf();
+  //   let priority = '';
+  //   if(event && event.value != "ALL"){
+  //     //priority = event.value;
+  //     priority = this.priorities.filter(v => v.valueOf() == event.value)[0];
+  //   }
+  //   console.log(priority);
+  //   this.refreshTodos.emit(priority);
+  // }
+
+  callRefreshTodos(event?: MatSelectChange): void {
+    let priority = '';
+    if (event) {
+      priority = this.priorities.find(v => v.valueOf() === event.value) || '';
+    }
+    this.refreshTodos.emit(priority);
+  }
+  
 
 }
--- TodoClientApp/src/app/components/todo-board/todo-board.component.ts
+++ TodoClientApp/src/app/components/todo-board/todo-board.component.ts
@@ -1,33 +1,30 @@
-import { Component,EventEmitter,OnInit,Output,inject } from '@angular/core';
-import { TodoItemComponent } from "../todo-item/todo-item.component";
-import { Todo } from '../../models/todo';
+import { Component, EventEmitter, OnInit, Output, inject } from '@angular/core';
+import { TodoItemComponent } from '../todo-item/todo-item.component';
+import { Priority, Todo } from '../../models/todo';
 import { CommonModule } from '@angular/common';
-import {
-  MatDialog,
-} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { CreateNewTodoComponent } from '../../dialogs/create-new-todo/create-new-todo.component';
 import { TodoService } from '../../services/todo.service';
-import { TodoBoardHeaderComponent } from "../todo-board-header/todo-board-header.component";
-import { TodoListComponent } from "../todo-list/todo-list.component";
+import { TodoBoardHeaderComponent } from '../todo-board-header/todo-board-header.component';
+import { TodoListComponent } from '../todo-list/todo-list.component';
 
 @Component({
   selector: 'app-todo-board',
   standalone: true,
   imports: [CommonModule, TodoBoardHeaderComponent, TodoListComponent],
   templateUrl: './todo-board.component.html',
-  styleUrl: './todo-board.component.css'
+  styleUrl: './todo-board.component.css',
 })
 export class TodoBoardComponent implements OnInit {
   readonly dialog = inject(MatDialog);
   today: Date = new Date();
   todos: Todo[] = [];
 
-  constructor(private todoService: TodoService) {
-      }
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
     this.today = new Date();
-    this.todoService.findAll().subscribe(data => {
+    this.todoService.findAll().subscribe((data) => {
       this.todos = data;
     });
   }
@@ -43,62 +40,28 @@ export class TodoBoardComponent implements OnInit {
     });
   }
 
-  refreshTodos(){
-    this.todoService.findAll().subscribe(data => {
-      this.todos = data;
-    });
-  }
-
-  deleteTodo(id: number){
-    alert("called from child");
-  }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // isMobile = false;
-  // isTablet = false;
-  // isDesktop = false;
-
-  // constructor(private breakpointObserver: BreakpointObserver, private todoService: TodoService) {
-  //   this.breakpointObserver.observe([
-  //     Breakpoints.Handset,
-  //     Breakpoints.Tablet,
-  //     Breakpoints.Web
-  //   ]).subscribe(result => {
-  //     this.isMobile = result.breakpoints[Breakpoints.Handset];
-  //     this.isTablet = result.breakpoints[Breakpoints.Tablet];
-  //     this.isDesktop = result.breakpoints[Breakpoints.Web];
+  // refreshTodos(priorityFilter?: Priority) {
+  //   console.log(priorityFilter?.valueOf());
+  //   this.todoService.findAll().subscribe((data) => {
+  //     this.todos = data;
+  //     if (priorityFilter) {
+  //       this.todos.filter(t => t.priority == priorityFilter.valueOf());
+  //     }
   //   });
   // }
+
+  refreshTodos(priorityFilter?: Priority): void {
+    this.todoService.findAll().subscribe((data) => {
+      this.todos = data;
+      if(priorityFilter && priorityFilter.toString().toUpperCase() !== 'ALL'){
+        console.log(typeof priorityFilter);
+          this.todos = this.todos.filter(t => t.priority === priorityFilter);
+      }
+    });
+  }
+  
+
+  deleteTodo(id: number) {
+    alert('called from child');
+  }
+}
