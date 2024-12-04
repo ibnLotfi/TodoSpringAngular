71e63b48cc1e0ba09a001d852ffa015c0e6257c8
ibnLotfi <ibnlotfi@hotmail.fr>
2024-12-04 18:45:30 +0100

    fix/cancel-button-dialog-create-todo

--- TodoClientApp/src/app/dialogs/create-new-todo/create-new-todo.component.ts
+++ TodoClientApp/src/app/dialogs/create-new-todo/create-new-todo.component.ts
@@ -1,29 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 import { MatOption } from '@angular/material/core';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { TodoService } from '../../services/todo.service';
 import { TodoCreateDto } from '../../models/todoCreateDto';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Priority } from '../../models/todo';
 
-
 @Component({
   selector: 'app-create-new-todo',
-  imports: [FormsModule,MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule,ReactiveFormsModule, MatOption, MatSelectModule],
+  imports: [
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatOption,
+    MatSelectModule,
+  ],
   templateUrl: './create-new-todo.component.html',
-  styleUrl: './create-new-todo.component.css'
+  styleUrl: './create-new-todo.component.css',
 })
 export class CreateNewTodoComponent implements OnInit {
+  public priorities = Object.values(Priority).filter(
+    (value) => typeof value === 'string'
+  );
 
-  public priorities = Object.values(Priority).filter(value => typeof value === 'string');
-
-  constructor(private todoService: TodoService, private dialogRef: MatDialogRef<CreateNewTodoComponent>) {
-  }
+  constructor(
+    private todoService: TodoService,
+    private dialogRef: MatDialogRef<CreateNewTodoComponent>
+  ) {}
 
   ngOnInit(): void {
     console.log(this.priorities);
@@ -32,17 +48,16 @@ export class CreateNewTodoComponent implements OnInit {
   form: FormGroup = new FormGroup({
     title: new FormControl(''),
     content: new FormControl(''),
-    priority: new FormControl('')
+    priority: new FormControl(''),
   });
 
-  onSubmit(){
+  onSubmit() {
     let todoToCreate: TodoCreateDto = {
       content: this.form.value.content,
-      lastEditDate: new Date,
+      lastEditDate: new Date(),
       title: this.form.value.title,
-      priority: this.form.value.priority
+      priority: this.form.value.priority,
     };
-
 
     this.todoService.save(todoToCreate).subscribe({
       next: () => {
@@ -50,9 +65,11 @@ export class CreateNewTodoComponent implements OnInit {
       },
       error: (err) => {
         console.error(err);
-      }
+      },
     });
-
   }
 
+  closeDialog() {
+    this.dialogRef.close();
+  }
 }
